8d79fe20375ae47a468a7783506fe8eb846a4f9b
Przemyslaw Duniec <pduniec@outlook.com>
2021-10-28 22:43:50 +0200

    Add icon to button panel

--- Unit2.pas
+++ Unit2.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ComCtrls, RzListVw,
-  Vcl.ExtCtrls, RzPanel;
+  Vcl.ExtCtrls, RzPanel, Vcl.Imaging.pngimage;
 
 type
   TForm2 = class(TForm)
